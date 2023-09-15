6575d4b1d90af8ac4619f0ffa18584dbc4343345
unknown <muofunanyajennifer1@gmail.com>
2023-09-15 16:42:49 +0100

    ensured the functionality of the CRUD operations

---
+++ controllers/personController.js
@@ -0,0 +1,73 @@
+const personModel = require('../models/personModel')
+
+// Create Person
+exports.createPerson = async (req, res) => {
+    const personInfo = req.body
+    try {
+        const existingEmail = await personModel.findOne({ name: personInfo.name })
+
+        if (existingEmail) {
+            res.status(409).json({ error: "Person data already exists" })
+            return
+        }
+
+        const personData = await personModel.create({ ...personInfo })
+        return res.status(200).json({ Success: true, message: personData })
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+}
+
+// Read Persons
+exports.getPerson = async (req, res) => {
+    const { name: name } = req.params
+    try {
+        const person = await personModel.findOne({ name });
+        if (!person) res.status(404).json({ success: false, message: "person does not exist" })
+        return res.status(200).json({ success: true, message: person })
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message })
+    }
+}
+// Read Persons
+exports.getAllPerson = async (req, res) => {
+    try {
+        const person = await personModel.find();
+        if (!person) res.status(404).json({ success: false, message: "person does not exist" })
+        return res.status(200).json({ success: true, message: person })
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message })
+    }
+}
+
+// Update Person
+exports.updatePerson = async (req, res) => {
+    try {
+        const user = { name: req.params.name };
+        const personInfo = req.body
+
+        const person = await personModel.findOneAndUpdate(user, personInfo, { new: true });
+
+        if (!person) {
+            return res.status(404).json({ message: 'Person not found' });
+        }
+        res.status(200).json({ success: true, message: person })
+
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message })
+    }
+
+}
+
+// Delete person
+exports.deletePerson = async (req, res) => {
+    const name = req.params.name
+
+    try {
+        const person = await personModel.findOneAndDelete({ name })
+        if (!person) res.status(404).json({ message: "Person does not exist" })
+        res.status(500).json({ success: true, message: "Person deleted successfully" })
+    } catch (err) {
+        res.status(500).json({ success: false, message: err })
+    }
+}
---
+++ routes/personRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+const validate = require('../middlewares/validateMiddleware')
+const {createPerson, getPerson, getAllPerson, updatePerson, deletePerson} = require('../controllers/personController')
+const {
+    createSchema,
+    updateSchema
+} = require('../schema/personSchema')
+
+router.post('/', validate(createSchema), createPerson)
+router.get('/', getAllPerson)
+router.get('/:name', getPerson)
+router.patch('/:name', validate(updateSchema), updatePerson)
+router.delete('/:name', deletePerson)
+
+module.exports = router;
